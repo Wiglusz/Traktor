584d0c71c6c8f78df070c59b06630145e8720438
Wiglusz <147412575+Wiglusz@users.noreply.github.com>
2024-01-08 11:39:17 +0100

    Add files via upload

---
+++ zad_2B_for.py
@@ -0,0 +1,22 @@
+def pomnoz_przez_dwa_for(input_list):
+    result = []
+    for number in input_list:
+        result.append(number * 2)
+    return result
+
+
+input_list = [1, 2, 3, 4, 5]
+result_for = pomnoz_przez_dwa_for(input_list)
+print(result_for)
+
+
+def pomnoz_przez_dwa_for(input_list):
+    result = []
+    for number in input_list:
+        result.append(number * 2)
+    return result
+
+
+input_list = [1, 2, 3, 4, 5]
+result_for = pomnoz_przez_dwa_for(input_list)
+print(result_for)
---
+++ zad_2D.py
@@ -0,0 +1,6 @@
+def co_drugi(input_list):
+    print(input_list[1::2])
+
+
+input_list = list(range(1, 11))
+co_drugi(input_list)
